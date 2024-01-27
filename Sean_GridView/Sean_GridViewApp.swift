21d34c6514abb485af0011c4df63bae45574f1a3
MohanedAzez <mohanedaliraqe90@gmail.com>
2024-01-27 14:55:34 +0300

    Initial Commit

---
+++ Sean_GridView/Sean_GridViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sean_GridViewApp.swift
+//  Sean_GridView
+//
+//  Created by Mohanned on 27/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Sean_GridViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
